81847a00a7f3a867239e9aa91077c702fa4daf11
Tilen Bobek <tilen.bobek@outfit7.com>
2018-12-26 09:40:56 +0100

    NEW: stochastics on daily, clear fields button, manual store into DB

--- utils.js
+++ utils.js
@@ -18,6 +18,17 @@ var utils = {
 		return date.getFullYear() + '-' + (date.getMonth()+1) + '-' + date.getDate();
 	},
 
+  dateToStrFull: function(date) {
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+
+    return "{0}-{1}-{2} {3}:{4}:{5}".format(year, month, day, hours, minutes, seconds);
+  },
+
 	getAddedDaysStr: function(dateStr, days) {
 		var date = new Date(dateStr);
 		date.setDate(date.getDate() + days);
