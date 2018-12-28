acbfbc410e8628a3392006e2702455335f3f21d0
Tilen Bobek <tilen.bobek@outfit7.com>
2018-12-28 22:51:04 +0100

    UPDATE: Simulate thrown out

--- MainControllerHelper.js
+++ MainControllerHelper.js
@@ -1,25 +1,2 @@
 var mainControllerHelper = {
-	readStockValues: function(seriesSrc, dateStr) {
-		var dateMs = new Date(dateStr).getTime();
-		var series = [];
-		for (var key in seriesSrc) {
-			timeMs = new Date(key).getTime();
-			if (timeMs > dateMs) {
-				var valuesSrc = seriesSrc[key];
-
-				var values = {};
-				values["date"] = key;
-				values["open"] = parseFloat(valuesSrc["1. open"]);
-				values["high"] = parseFloat(valuesSrc["2. high"]);
-				values["low"] = parseFloat(valuesSrc["3. low"]);
-				values["close"] = parseFloat(valuesSrc["4. close"]);
-				values["volume"] = parseFloat(valuesSrc["5. volume"]);
-
-				series.push(values);
-			} else {
-				break;
-			}
-		}
-		return series;
-	}
 };
