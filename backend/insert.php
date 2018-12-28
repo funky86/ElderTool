93a86a250a4a23e2022a7c821b82262eeb975ba8
Tilen Bobek <tilen.bobek@outfit7.com>
2018-12-28 20:05:49 +0100

    NEW: exit_date added, date_time used for entry_date

--- backend/insert.php
+++ backend/insert.php
@@ -5,27 +5,29 @@
 require('utils.php');
 
 $symbol = $_GET['symbol'];
-$date_time = $_GET['date_time'];
+$entry_date = $_GET['entry_date'];
 $entry_price = $_GET['entry_price'];
+$exit_date = $_GET['exit_date'];
 $exit_price = $_GET['exit_price'];
 
-if (!isset($symbol) || !isset($date_time) || !isset($entry_price) || !isset($exit_price)) {
-	echo -1;
+if (!isset($symbol) || !isset($entry_date) || !isset($entry_price) || !isset($exit_date) || !isset($exit_price)) {
+	echo '-1';
 	exit();
 }
 
 $conn = getDatabaseConnection();
 
-$query = "INSERT INTO trades (symbol, date_time, entry_price, exit_price) VALUES (?, ?, ?, ?)";
+$query = "INSERT INTO trades (symbol, entry_date, entry_price, exit_date, exit_price) VALUES (?, ?, ?, ?, ?)";
 
-$params = array($symbol, new DateTime($date_time), $entry_price, $exit_price);
+$params = array($symbol, new DateTime($entry_date), $entry_price, new DateTime($exit_date), $exit_price);
 
 $stmt = sqlsrv_query($conn, $query, $params);
 
 if ($stmt === false) {
-	die(print_r(sqlsrv_errors(), true));
+	echo '-2';
+	//die(print_r(sqlsrv_errors(), true));
+} else {
+	echo '0';
 }
-
-echo '0';
 
 ?>
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -2,7 +2,8 @@ DROP TABLE IF EXISTS dbo.trades;
 
 CREATE TABLE trades (
     symbol varchar(8),
-    date_time datetime,
+    entry_date datetime,
     entry_price float(24),
+    exit_date datetime,
     exit_price float(24)
 );
